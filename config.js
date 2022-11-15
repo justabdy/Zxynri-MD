5955d20179cfc2df8594f6cc7aeed720046e3444
justabdy <92249237+justabdy@users.noreply.github.com>
2022-11-15 17:16:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,31 +5,31 @@
 // RECODE BY ZIFABOTZ
 
 //=============『 Utama 』================== //
-global.owner = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
-global.mods = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
-global.prems = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
+global.owner = ['6287789210430'] //ganti pakai nomormu biar bot berfungsi
+global.mods = ['6287789210430'] //ganti pakai nomormu biar bot berfungsi
+global.prems = ['6287789210430'] //ganti pakai nomormu biar bot berfungsi
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'ᴹᴿ᭄ DaniBotzOffcོ_1ঔৣ꧂'
-global.numberowner = '6281251549899' //ganti pakai nomor mu
-global.instagram = 'Gk Ad Bwang:v' //ganti pakai Instagram mu
-global.github = 'https://github.com/Bot-Mdnew' //ganti aja pakai githubmu
+global.nameowner = 'abdy.'
+global.numberowner = '6287789210430' //ganti pakai nomor mu
+global.instagram = '@abdyckk' //ganti pakai Instagram mu
+global.github = 'https://github.com/justabdy' //ganti aja pakai githubmu
 global.tt = '"_"' //ganti aja pakai tiktok mu
 global.sc = 'https://youtu.be/lMcwJMp_0rM' //jngn ganti hargai creator
 global.ss = 'https://youtu.be/3m7tRD4RklM' //jngn ganti hargai creator
 global.sss = 'https://youtu.be/PzUH1ApMCig' //jngn ganti hargai creator
 global.yt = 'https://youtube.com/channel/UCeUvY_fIVSAaQ1n5SAylI-A'
-global.dana = '081251549899' //ganti pakai nomor dana mu
-global.pulsa = '081352848936' //ganti pakai nomor pulsa mu
-global.gopay = '081352848936' //ganti pakai nomor Gopay mu
+global.dana = '087789210430' //ganti pakai nomor dana mu
+global.pulsa = '087789210430' //ganti pakai nomor pulsa mu
+global.gopay = '087789210430' //ganti pakai nomor Gopay mu
 
 //=============『 Info Bot 』=================//
-global.namebot = '❋ཻུ۪۪⸙Danibotz-Ofc⳹ ❋ཻུ۪۪' //ganti jadiin nama bot mu
-global.gc = 'https://chat.whatsapp.com/BiJs5Xl4oJA1Ama5BahUwd' //ganti pakai grup mu
-global.web = 'https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.webm = '"_"' //ganti pakai Instagram mu
-global.saweria = '"_"' //bebas ganti aja pakai apa
-global.nomer = 'https://wa.me/6281251549899' //ganti jadi nomor mu
+global.namebot = 'cry.' //ganti jadiin nama bot mu
+global.gc = 'https://chat.whatsapp.com/JxI53XkvnnYHP2D1eIZ8fu' //ganti pakai grup mu
+global.web = 'https://instagram.com/abdyckk' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.webm = '"https://instagram.com/abdyckk"' //ganti pakai Instagram mu
+global.saweria = '"https://www.paypal.me/gkdls"' //bebas ganti aja pakai apa
+global.nomer = 'https://wa.me/6287789210430' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERMANEN Rp 10.000\nMAKSIMAL 2 GRUP\n》PERMANEN  Rp 20.000\nMAKSIMAL 6 GRUP \n》1 BULAN Rp 5.000\nMAKSIMAL 1 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -46,8 +46,8 @@ global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = '❋ཻུ۪۪Danibotz-Ofc⳹ ❋ཻུ۪۪'
 global.watermark = wm
-global.wm2 = 'ᯤ creator by Oᵂηʀ᭄๖ۣۣۜDani_ofcོ1꧅'
-global.wm3 = 'ᯤ Dani Offcial'
+global.wm2 = 'ᯤ creator by abdy.'
+global.wm3 = 'ᯤ abdy.'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
